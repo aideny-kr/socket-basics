9c55b45ad67844f1f6936551f8ca8505356ee518
Chan Yi <huichanyi@gmail.com>
2016-09-11 21:00:38 -0400

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -5,4 +5,8 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('socket.io Server Connected');
-})
+});
+
+socket.on('message', function(message){
+    console.log('New message', message.text);
+});
